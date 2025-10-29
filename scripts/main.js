8cfb703cff1668a7ae7bbfee653466d2a1807fac
Konstantin Kustov <oxy949@gmail.com>
2025-10-29 14:07:50 +0300

    Simplifies GinzzzuBooApp class
    Removes unused methods and context menus from the GinzzzuBooApp class.
    
    This change streamlines the class by removing methods that are no longer
    in use, reducing code complexity and improving maintainability.

--- scripts/main.js
+++ scripts/main.js
@@ -201,65 +201,6 @@ export class GinzzzuBooApp extends HandlebarsApplicationMixin(ApplicationV2) {
         }
     }
 
-    getIcon(id, type) {
-        if (MonksCommonDisplay.selectToken == type)
-            return "fa-bullseye";
-
-        if (id == "combat") // && game.combats.active)
-            return "fa-swords";
-        else if (id == "gm" || !id)
-            return "fa-people-arrows";
-        else if (id == "party")
-            return "fa-users-viewfinder";
-        else if (id == "controlled")
-            return "fa-street-view";
-        else if (id == "scene")
-            return "fa-presentation-screen";
-
-        return "fa-users";
-    }
-
-    getImage(id, type) {
-        if (MonksCommonDisplay.selectToken == type)
-            return null;
-
-        if (id != "combat" && id != "gm") {
-            //try and find the image of the token
-            if (id.indexOf(",") > -1)
-                return null;
-
-            let token = canvas.scene.tokens.find(t => t.id == id || t.actor?.id == id);
-            if (token)
-                return token.texture.src;
-        }
-        return null;
-    }
-
-    getTooltip(id, type) {
-        if (MonksCommonDisplay.selectToken == type)
-            return "Selecting an Actor";
-
-        if (id == "combat") // && game.combats.active)
-            return game.i18n.localize("MonksCommonDisplay.Combatant");
-        else if (id == "gm" || !id)
-            return game.i18n.localize("MonksCommonDisplay.GM");
-        else if (id == "party")
-            return game.i18n.localize("MonksCommonDisplay.Party");
-        else if (id == "controlled")
-            return game.i18n.localize("MonksCommonDisplay.Controlled");
-        else if (id == "scene")
-            return game.i18n.localize("MonksCommonDisplay.FullScene");
-
-        if (id.indexOf(",") > -1)
-            return null;
-
-        let token = canvas.scene.tokens.find(t => t.id == id || t.actor?.id == id);
-        if (token)
-            return token.name;
-
-        return "";
-    }
-
     getPos() {
         this.pos = game.user.getFlag(MODULE_ID, "position");
 
@@ -312,176 +253,11 @@ export class GinzzzuBooApp extends HandlebarsApplicationMixin(ApplicationV2) {
         } catch (e) { /* ignore */ }
     }
 
-    static clearJournals() {
-        MonksCommonDisplay.emit("closeJournals");
-    }
-
-    static clearImage() {
-        MonksCommonDisplay.emit("closeImagePopout");
-    }
-
-    static async toggleScreen() {
-        if (!!MonksCommonDisplay.selectToken) {
-            let tokenids = canvas.tokens.controlled.map((t) => t.id).join(",");
-            if (setting("per-scene")) {
-                await canvas.scene.setFlag(MODULE_ID, MonksCommonDisplay.selectToken, tokenids);
-                foundry.utils.setProperty(canvas.scene, `flags.${MODULE_ID}.${MonksCommonDisplay.selectToken}`, tokenids);
-            } else {
-                await game.settings.set(MODULE_ID, MonksCommonDisplay.selectToken, tokenids);
-            }
-            if (MonksCommonDisplay.selectToken == "screen") MonksCommonDisplay.screenChanged(); else MonksCommonDisplay.focusChanged();
-
-            MonksCommonDisplay.selectToken = null;
-        } else {
-            let active = !setting("screen-toggle");
-            await game.settings.set(MODULE_ID, "screen-toggle", active);
-            if (active) {
-                MonksCommonDisplay.screenChanged();
-            }
-        }
-        this.render();
-    }
-
-    static async toggleFocus() {
-        let active = !setting("focus-toggle");
-        await game.settings.set(MODULE_ID, "focus-toggle", active);
-        MonksCommonDisplay.focusChanged();
-        this.render();
-    }
-
     _createContextMenus() {
-        this._createContextMenu(this._getContextOptions, ".common-button-group", {
-            fixed: true,
-            hookName: "getCommonDisplayContextOptions",
-            parentClassHooks: false
-        });
-        this._createContextMenu(this._getContextOptions, ".common-button-group .header", {
-            fixed: true,
-            hookName: "getCommonDisplayContextOptions",
-            parentClassHooks: false,
-            eventName: "click",
-        });
     }
 
     _getContextOptions() {
-        return [
-            {
-                name: game.i18n.localize("MonksCommonDisplay.GM"),
-                icon: '<i class="fas fa-user"></i>',
-                condition: (btn) => {
-                    return game.user.isGM && btn.closest(".common-button-group").dataset.group == "screen"
-                },
-                callback: async (btn) => {
-                    let group = btn.closest(".common-button-group").dataset.group;
-                    MonksCommonDisplay.selectToken = null;
-                    if (setting("per-scene"))
-                        await canvas.scene.setFlag(MODULE_ID, group, "gm");
-                    else
-                        await game.settings.set(MODULE_ID, group, "gm");
-                    if (group == "screen") MonksCommonDisplay.screenChanged(); else MonksCommonDisplay.focusChanged();
-                    this.render(true);
-                }
-            },
-            {
-                name: game.i18n.localize("MonksCommonDisplay.Controlled"),
-                icon: '<i class="fas fa-street-view"></i>',
-                condition: game.user.isGM,
-                callback: async (btn) => {
-                    let group = btn.closest(".common-button-group").dataset.group;
-                    MonksCommonDisplay.selectToken = null;
-                    if (setting("per-scene"))
-                        await canvas.scene.setFlag(MODULE_ID, group, "controlled");
-                    else
-                        await game.settings.set(MODULE_ID, group, "controlled");
-                    if (group == "screen") MonksCommonDisplay.screenChanged(); else MonksCommonDisplay.focusChanged();
-                    this.render(true);
-                }
-            },
-            {
-                name: game.i18n.localize("MonksCommonDisplay.FullScene"),
-                icon: '<i class="fas fa-presentation-screen"></i>',
-                condition: (btn) => {
-                    return game.user.isGM && btn.closest(".common-button-group").dataset.group == "screen";
-                },
-                callback: async (btn) => {
-                    let group = btn.closest(".common-button-group").dataset.group;
-                    MonksCommonDisplay.selectToken = null;
-                    if (setting("per-scene"))
-                        await canvas.scene.setFlag(MODULE_ID, group, "scene");
-                    else
-                        await game.settings.set(MODULE_ID, group, "scene");
-                    if (group == "screen") MonksCommonDisplay.screenChanged(); else MonksCommonDisplay.focusChanged();
-                    this.render(true);
-                }
-            },
-            {
-                name: game.i18n.localize("MonksCommonDisplay.Combatant"),
-                icon: '<i class="fas fa-swords"></i>',
-                condition: game.user.isGM,
-                callback: async (btn) => {
-                    let group = btn.closest(".common-button-group").dataset.group;
-                    MonksCommonDisplay.selectToken = null;
-                    if (setting("per-scene"))
-                        await canvas.scene.setFlag(MODULE_ID, group, "combat");
-                    else
-                        await game.settings.set(MODULE_ID, group, "combat");
-                    if (group == "screen") MonksCommonDisplay.screenChanged(); else MonksCommonDisplay.focusChanged();
-                    this.render(true);
-                }
-            },
-            {
-                name: game.i18n.localize("MonksCommonDisplay.Party"),
-                icon: '<i class="fas fa-users-viewfinder"></i>',
-                condition: (btn) => {
-                    return game.user.isGM && btn.closest(".common-button-group").dataset.group == "screen";
-                },
-                callback: async (btn) => {
-                    let group = btn.closest(".common-button-group").dataset.group;
-                    MonksCommonDisplay.selectToken = null;
-                    if (setting("per-scene"))
-                        await canvas.scene.setFlag(MODULE_ID, group, "party");
-                    else
-                        await game.settings.set(MODULE_ID, group, "party");
-                    if (group == "screen") MonksCommonDisplay.screenChanged(); else MonksCommonDisplay.focusChanged();
-                    this.render(true);
-                }
-            },
-            {
-                name: game.i18n.localize("MonksCommonDisplay.SelectTokens"),
-                icon: '<i class="fas fa-bullseye"></i>',
-                condition: game.user.isGM,
-                callback: btn => {
-                    let group = btn.closest(".common-button-group").dataset.group;
-                    MonksCommonDisplay.selectToken = (!!MonksCommonDisplay.selectToken ? null : group);
-                    this.render(true);
-                }
-            }
-        ];
-    }
-
-    async updateToken(tkn, refresh = true) {
-        let diff = {};
-
-        if (tkn.img != (tkn.token.actor.img || tkn.token.texture.src)) {
-            diff.img = (tkn.token.actor.img || tkn.token.texture.src);
-            let thumb = this.thumbnails[diff.img];
-            if (!thumb) {
-                try {
-                    thumb = await ImageHelper.createThumbnail(diff.img, { width: 50, height: 50 });
-                    this.thumbnails[diff.img] = (thumb?.thumb || thumb);
-                } catch {
-                    thumb = 'icons/svg/mystery-man.svg';
-                }
-            }
-
-            diff.thumb = (thumb?.thumb || thumb);
-        }
-
-        if (Object.keys(diff).length > 0) {
-            foundry.utils.mergeObject(tkn, diff);
-            if (refresh)
-                this.render();
-        }
+        return [];
     }
 }
 
@@ -495,7 +271,7 @@ let _appInstance = null;
 
 Hooks.on('renderSceneControls', async (control, html, data) => {
   if (game.user.isGM && $('#scene-controls-layers .ginzzzu-boo-display', html).length == 0) {
-    const name = 'monkscommondisplay';
+    const name = 'ginzzzudisplay';
     const title = game.i18n.localize("GINZZZUBOO.ToggleToolbar");
     const icon = 'fas fa-music';
     const active = setting('show-toolbar');
@@ -538,9 +314,6 @@ const _reRenderApp = foundry.utils.debounce(() => {
     } catch (e) { /* ignore */ }
 }, 250);
 
-Hooks.on('createUser', _reRenderApp);
-Hooks.on('updateUser', _reRenderApp);
-Hooks.on('deleteUser', _reRenderApp);
 Hooks.on('userConnected', _reRenderApp);
 Hooks.on('userDisconnected', _reRenderApp);
 
